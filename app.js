e667a13389c9eed811641ddfba24102e4f9c0d84
André Leodoro <aleodoro93@gmail.com>
2023-08-28 21:41:46 -0300

    feat:adição das propriedades do readline input e output

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+import * as readline from 'node:readline/promises';
+import {stdin as input, stdout as output} from 'node:process'
+
 class ListaPropriedades {
     constructor() {
         this.propCss = [];
+        this.rl = createInterface({
+            input: ProcessingInstruction.stdin,
+            output: ProcessingInstruction.stdout
+        });
     }
 }
